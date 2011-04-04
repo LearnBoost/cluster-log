e03d5d345a7a0b4c85b30670bb3c232f0b49f61e
Tj Holowaychuk <tj@vision-media.ca>
2011-04-04 12:09:17 -0700

    make example fail at random

--- example.js
+++ example.js
@@ -4,8 +4,16 @@
  */
 
 var cluster = require('cluster')
+  , log = require('./')
   , http = require('http');
 
 var server = http.createServer(function(req, res){
+  if (Math.random() > 0.9) throw new Error('omgz!'); 
   res.end('Hello World');
 });
+
+cluster(server)
+  .use(log())
+  .listen(3000);
+
+console.log('cluster listening on port 3000');
